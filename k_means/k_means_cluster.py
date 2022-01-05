26427138d700c2c2d0091d59f9504d9870e2d8fc
Pankaj Jha <jha3@llnl.gov>
2022-01-04 21:12:07 -0800

    Update code for k-means clustering

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -30,7 +30,8 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.show()
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+#data_file = open("../final_project/final_project_dataset.pkl", "rb")
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
@@ -81,12 +82,12 @@ plt.show()
 from sklearn.cluster import KMeans
 kmeans = KMeans(n_clusters=2, random_state=0)
 kmeans.fit(finance_features)
-print "k-means labels: ", kmeans.labels_
-print "k-means cluster centers: ", kmeans.cluster_centers_
+print ("k-means labels: ", kmeans.labels_)
+print ("k-means cluster centers: ", kmeans.cluster_centers_)
 
 pred = kmeans.predict(finance_features)
 
-print "k-means labels - pred: ", kmeans.labels_ - pred
+print ("k-means labels - pred: ", kmeans.labels_ - pred)
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
@@ -95,7 +96,7 @@ plt.figure()
 try:
     Draw(pred, finance_features[:, [0, 1]], poi, mark_poi=False, name="clusters1.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print ("no predictions object named pred found, no clusters to plot")
   
 """
 plt.figure()
