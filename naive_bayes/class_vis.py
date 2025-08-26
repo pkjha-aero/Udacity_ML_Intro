804f34c9ed2989400f3b2ded8b7405a25c159cb7
Pankaj Jha <jha3@llnl.gov>
2025-08-25 20:55:50 -0700

    Fix a byte encoding stuff

--- naive_bayes/class_vis.py
+++ naive_bayes/class_vis.py
@@ -62,5 +62,5 @@ def output_image(name, format, bytes):
     data = {}
     data['name'] = name
     data['format'] = format
-    data['bytes'] = base64.encodestring(bytes)
+    #data['bytes'] = base64.encodestring(bytes)
     print(image_start+json.dumps(data)+image_end)
